4041f3cf268bab259322f6aab145562a47c4da2c
Andrii <khaliavkinandrii@gmail.com>
2023-12-05 20:50:16 +0100

    variables

--- JS-Fundamentals/lesson1.js
+++ JS-Fundamentals/lesson1.js
@@ -1,2 +1,26 @@
 // 1.Hello World
 console.log("Hello World!")
+
+// Variables
+
+var firstName = "Andrii"
+let lastName = "Khal"
+
+console.log(firstName)
+
+var age, dateOfBirth, sex
+age = 30
+dateOfBirth = "25/11/1993"
+sex = "m"
+
+console.log(age)
+age = 29
+console.log(age)
+
+// constants
+const  occepation = "QA Engineer"
+
+console.log(occepation)
+
+// data types
+
