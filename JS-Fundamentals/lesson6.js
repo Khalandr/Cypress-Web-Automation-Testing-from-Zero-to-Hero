ab3ce8df9c168680027bf85906bfe5da2ce5bcf2
Andrii <khaliavkinandrii@gmail.com>
2023-12-05 23:11:20 +0100

    conditional statement

---
+++ JS-Fundamentals/lesson6.js
@@ -0,0 +1,16 @@
+//Conditional statement
+
+// if(condition){
+//     //execute some code
+// } else {
+//     //execute some code
+// }
+
+let hour = 5
+if (hour >= 6 && hour < 12){
+    console.log("Good Morning")
+} else if (hour >= 12 && hour < 18){
+    console.log("Good afternoon")
+} else {
+    console.log("Good Evening")
+}
