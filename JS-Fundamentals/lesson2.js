f0776450f79b5411a722f41503af98d8519252d5
Andrii <khaliavkinandrii@gmail.com>
2023-12-05 21:17:09 +0100

    concatination and interpolation

---
+++ JS-Fundamentals/lesson2.js
@@ -0,0 +1,7 @@
+//Concatination and Interpolation
+let product = "Cup"
+let price = 50
+let messageToPrint = "The price for your " + product + " is " + price + " dollars" //concatination
+let messageToPrint2 = `The price for your ${product} is ${price} dollars` //interpolation
+console.log(messageToPrint)
+console.log(messageToPrint2)
